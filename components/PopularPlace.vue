c491223835228cb42a6de25f0a1845d336db8f19
Edmund Hee <edmund.hee05@gmail.com>
2020-09-18 15:40:50 +0800

    Finished Webinar Resources

---
+++ components/PopularPlace.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="flex flex-col w-screen h-full text-gray-800 font-poppins py-10 px-16">
+        <div class="text-xl">Popular places recommended by local</div>
+        <div class="grid grid-cols-3 gap-2">
+            <div class="flex items-center my-6 p-2">
+                <img class="h-20 object-cover rounded-lg" src="https://images.pexels.com/photos/1031780/pexels-photo-1031780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                <div class="flex flex-col px-2">
+                    <div class="text-sm">
+                        Penang - George Town
+                    </div>
+                    <div class="text-xs">
+                        RM 99/night (avg.)
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center my-6 p-2">
+                <img class="h-20 object-cover rounded-lg" src="https://images.pexels.com/photos/1050597/pexels-photo-1050597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                <div class="flex flex-col px-2">
+                    <div class="text-sm">
+                        Kota Kinabalu
+                    </div>
+                    <div class="text-xs">
+                        RM 199/night (avg.)
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center my-6 p-2">
+                <img class="h-20 object-cover rounded-lg" src="https://images.pexels.com/photos/1835712/pexels-photo-1835712.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                <div class="flex flex-col px-2">
+                    <div class="text-sm">
+                        Redang Island
+                    </div>
+                    <div class="text-xs">
+                        RM 299/night (avg.)
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center my-6 p-2">
+                <img class="h-20 object-cover rounded-lg" src="https://images.pexels.com/photos/1125979/pexels-photo-1125979.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                <div class="flex flex-col px-2">
+                    <div class="text-sm">
+                        Tioman Island
+                    </div>
+                    <div class="text-xs">
+                        RM 299/night (avg.)
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center my-6 p-2">
+                <img class="h-20 object-cover rounded-lg" src="https://images.pexels.com/photos/2131967/pexels-photo-2131967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                <div class="flex flex-col px-2">
+                    <div class="text-sm">
+                        Sekinchan
+                    </div>
+                    <div class="text-xs">
+                        RM 199/night (avg.)
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center my-6 p-2">
+                <img class="h-20 object-cover rounded-lg" src="https://images.pexels.com/photos/67559/kuala-lumpur-petronas-twin-towers-malaysia-klcc-67559.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                <div class="flex flex-col px-2">
+                    <div class="text-sm">
+                        Kuala Lumpur
+                    </div>
+                    <div class="text-xs">
+                        RM 299/night (avg.)
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
